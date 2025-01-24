9736afabba71266f44692ed96cd8983ee6909c10
oussamasine07 <sineoussama@gmail.com>
2025-01-24 10:21:12 +0100

    make current locations functionality

--- js/app.js
+++ js/app.js
@@ -1,15 +1,35 @@
 
 let result;
 const icon = document.getElementById("icon");
+const currentLocationBtn = document.getElementById("current-location")
 
-async function getWeather () {
-    const weather = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=-6.353335&lon=32.334193&appid=dd235072273e8b3d0e412e7e1d4435b7");
+const currentLocation = {};
 
-    result = await weather.json();
-
-    
+const setLocation = (position) => {
+    currentLocation.longitude = position.coords.longitude;
+    currentLocation.latitude = position.coords.latitude;
 }
 
-getWeather();
+const getCurrentLocationWeather = () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(setLocation);
+    } else {
+        console.log("this browser does not support localization")
+    }
+}
+
+
+
+currentLocationBtn.addEventListener("click", getCurrentLocationWeather);
+
+// async function getWeather () {
+//     const weather = await fetch("https://api.openweathermap.org/data/2.5/weather?lat=-6.353335&lon=32.334193&appid=dd235072273e8b3d0e412e7e1d4435b7");
+
+//     result = await weather.json();
+
+    
+// }
+
+// getWeather();
 
 
