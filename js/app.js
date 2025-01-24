111f03d01b64b0723528165d7d540d359ede8a58
oussamasine07 <sineoussama@gmail.com>
2025-01-24 11:46:54 +0100

    declare needed elements in js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,24 @@
 
 // get all DOM elements
 const currentLocationBtn = document.getElementById("current-location")
-
+const mainSideIcon = document.getElementById("main-side-icon");
+const mainSideDegree = document.getElementById("main-side-degree");
+const mainSideDescription = document.getElementById("main-side-description");
+const countery = document.getElementById("countery");
+const city = document.getElementById("city");
+const lat = document.getElementById("lat");
+const lon = document.getElementById("lon");
+const sunRise = document.getElementById("sun-rise");
+const sunSet = document.getElementById("sun-set");
+const feelLike = document.getElementById("feel-like");
+const windSpeed = document.getElementById("wind-speed");
+const windDegree = document.getElementById("wind-degree");
+const maxTemp = document.getElementById("max-temp");
+const minTemp = document.getElementById("min-temp");
+const presure = document.getElementById("presure");
+const humidity = document.getElementById("humidity");
+const seaLevel = document.getElementById("sea-level");
+const grndLevel = document.getElementById("grnd-level");
 
 const apiKey = "dd235072273e8b3d0e412e7e1d4435b7";
 
